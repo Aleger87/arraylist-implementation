35496604671835099853df2536b0e8d629d33652
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-12-08 00:56:48 +0300

    Initial commit

---
+++ src/main/java/org/example/StringListImpl.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringListImpl implements StringList{
+    //protected transient int modCount = 0;
+    private int size;
+    private List<String> stringList = new ArrayList<>();
+
+    @Override
+    public String add(String item) {
+        stringList.add(item);
+        size++;
+        return item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+}
