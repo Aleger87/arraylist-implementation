5552ff23295c8d9b6ecd8f5ba875d21ef369b00f
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-12-11 00:52:49 +0300

    v1 StringList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,67 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringListImpl stringList = new StringListImpl(3);
+        System.out.println(stringList.toString());
+
+        stringList.add("test");
+        System.out.println(stringList.toString());
+
+        stringList.set(2, "test2");
+        System.out.println(stringList.toString());
+
+        stringList.add("test3");
+        System.out.println(stringList.toString());
+
+        stringList.remove("test3");
+        System.out.println(stringList.toString());
+
+        stringList.remove(2);
+        System.out.println(stringList.toString());
+
+        if (stringList.contains("test")) {
+            System.out.println("есть в списке");
+        }
+
+        stringList.set(2, "test2");
+        System.out.println(stringList.get(2));
+
+
+        stringList.size();
+
+        stringList.isEmpty();
+
+        stringList.clear();
+        stringList.add(2,"test");
+        System.out.println(stringList.toString());
+        stringList.toArray();
+        stringList.add("sss");
+        System.out.println(stringList);
+
+        System.out.println(stringList.get(0));
+
+        StringListImpl stringList1 = new StringListImpl(3);
+        if (stringList.equals(stringList1)) {
+            System.out.println("есть");
+        }
+
+        stringList.toArray();
+        System.out.println(stringList);
+
+        stringList.addPlus("rrr");
+        System.out.println(stringList);
+        stringList.addPlus("lll");
+        System.out.println(stringList);
+
+       // stringList.remove("lll");
+        //System.out.println(stringList);
+        stringList.removeMinus("lll");
+        System.out.println(stringList);
+
+
+
     }
 }
--- src/test/java/org/example/StringListImplTest.java
+++ src/test/java/org/example/StringListImplTest.java
@@ -1,62 +1,249 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class StringListImplTest {
+    private  StringListImpl stringListImpl = new StringListImpl(3);
+    private  String[] stringList = new String[3];
+    private boolean ifResult = false;
 
-    @org.junit.jupiter.api.Test
+    @BeforeEach
+    public  void setUp() {
+        stringList[0] = "test";
+        stringList[1] = "test1";
+        stringList[2] = "test2";
+
+        stringListImpl.add("test");
+        stringListImpl.add("test1");
+        stringListImpl.add("test2");
+    }
+
+    @Test
     void add() {
+       Assertions.assertEquals(stringListImpl.toString(), Arrays.toString(stringList));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void addPlus() {
+        String string = "Newt_test";
+        String[] strings = Arrays.copyOf(stringList, stringList.length + 1);
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i]==null) {
+                strings[i] = string;
+            }
+        }
+        stringListImpl.addPlus(string);
+        Assertions.assertEquals(stringListImpl.toString(),Arrays.toString(strings));
+    }
+
+    @Test
     void testAdd() {
+        int index = 0;
+        String string = "test4";
+        stringListImpl.add(index, string);
+        stringList[index] = string;
+        Assertions.assertEquals(stringListImpl.toString(), Arrays.toString(stringList));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test()
+    void testAddIndexOutOfBoundsException() {
+        int index = 3;
+        String string = "test4";
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+                stringListImpl.add(index, string);
+        });
+
+    }
+
+    @Test
     void set() {
+        testAdd();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test()
+    void testSetIndexOutOfBoundsException() {
+        testAddIndexOutOfBoundsException();
+    }
+
+    @Test
     void remove() {
+        String string = "test1";
+        for (int i = 0; i < stringList.length; i++) {
+            if (stringList[i].equals(string)) {
+                stringList[i] = null;
+            }
+        }
+        stringListImpl.remove(string);
+        Assertions.assertEquals(stringListImpl.toString(), Arrays.toString(stringList));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testRemoveIllegalArgumentException() {
+        String string = "test4";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            stringListImpl.remove(string);
+        });
+    }
+
+    @Test
     void testRemove() {
+        int index = 0;
+        stringList[index] = null;
+        stringListImpl.remove(0);
+        Assertions.assertEquals(stringListImpl.toString(), Arrays.toString(stringList));
+
     }
 
-    @org.junit.jupiter.api.Test
-    void contains() {
+    @Test
+    void removeMinus() {
+        String string = "test";
+        for (int i = 0; i < stringList.length; i++) {
+            if (stringList[i] == string) {
+                stringList[i] = null;
+            }
+        }
+        List<String> arrayList = new ArrayList<>(Arrays.stream(stringList).toList());
+        arrayList.removeIf(Objects::isNull);
+       String[] stringListNew = new String[arrayList.size()];
+        arrayList.toArray(stringListNew);
+        stringListImpl.removeMinus(string);
+        Assertions.assertEquals(stringListImpl.toString(), Arrays.toString(stringListNew));
+
     }
 
-    @org.junit.jupiter.api.Test
-    void indexOf() {
+    @Test
+    void testRemoveIndexOutOfBoundsException() {
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            stringListImpl.remove(5);
+        });
+
     }
 
-    @org.junit.jupiter.api.Test
-    void lastIndexOf() {
+    @ParameterizedTest
+    @MethodSource("parametersForTests")
+    void contains(String string) {
+        boolean expected = stringListImpl.contains(string);
+        Assertions.assertTrue(expected);
     }
 
-    @org.junit.jupiter.api.Test
+    @ParameterizedTest
+    @MethodSource("parametersForTests")
+    void indexOf(String string) {
+        Integer actualIndex= -1;
+        for (int i = 0; i < stringList.length; i++) {
+            if (stringList[i].equals(string)) {
+                actualIndex = i;
+                return;
+            }
+        }
+        Integer expectedIndex = stringListImpl.indexOf(string);
+
+        Assertions.assertEquals(actualIndex, expectedIndex);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("parametersForTests")
+    void lastIndexOf(String string) {
+        Integer actualIndex= -1;
+        for (int i = stringList.length-1; i >= 0; i--) {
+            if (stringList[i].equals(string)) {
+                actualIndex = i;
+                return;
+            }
+        }
+        Integer expectedIndex = stringListImpl.lastIndexOf(string);
+
+        Assertions.assertEquals(actualIndex, expectedIndex);
+    }
+
+    @Test
     void get() {
+        int index = 0;
+        String actual = stringList[index];
+        String expected = stringListImpl.get(index);
+        Assertions.assertEquals(actual, expected);
     }
 
-    @org.junit.jupiter.api.Test
-    void testEquals() {
+    @Test
+    void testGetIndexOutOfBoundsException() {
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            stringListImpl.get(5);
+        });
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testEqualsIllegalArgumentException() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            stringListImpl.equals(null);
+        });
+    }
+    @Test
+    void testEqualsTrue() {
+        StringListImpl otherList = new StringListImpl(3);
+       Assertions.assertTrue(stringListImpl.equals(otherList));
+    }
+    @Test
+    void testEqualsFalse() {
+        Assertions.assertFalse(stringListImpl.equals(stringList));
+    }
+
+    @Test
     void size() {
+        int actual = stringList.length;
+        int expected = stringListImpl.size();
+        Assertions.assertEquals(actual, expected);
     }
 
-    @org.junit.jupiter.api.Test
-    void isEmpty() {
+    @Test
+    void isEmptyFalse() {
+        Assertions.assertFalse(stringListImpl.isEmpty());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void isEmptyTrue() {
+        StringListImpl stringListImplTrue = new StringListImpl(3);
+        stringListImplTrue.add("");
+        stringListImplTrue.add("");
+        stringListImplTrue.add("");
+        Assertions.assertTrue(stringListImplTrue.isEmpty());
+    }
+
+    @Test
     void clear() {
+        Arrays.fill(stringList, null);
+        stringListImpl.clear();
+        Assertions.assertEquals(Arrays.toString(stringList), stringListImpl.toString());
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void toArray() {
+        String[] stringsActual = new String[0];
+        String[] stringExpected = stringListImpl.toArray();
+        Assertions.assertEquals(Arrays.toString(stringsActual), Arrays.toString(stringExpected));
+
+    }
+
+    public static Stream<Arguments> parametersForTests() {
+        return Stream.of(
+                Arguments.of("Test"),
+                Arguments.of("TEST"),
+                Arguments.of("teST")
+        );
     }
 }
